e4008200e43a797e6b40486ff9a7b302c23f039a
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-01 14:02:30 +0300

    define the routes

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/sophiewanjiku/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("UPDATE")
+	router.HandleFunc("/book{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
