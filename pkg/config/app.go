69ea72e086e44e464047a7baebd811dda9347714
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-01 14:03:28 +0300

    database

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,5 +1,18 @@
-package main
+package config
 
-func main() {
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func connect() {
+	d, err := gorm.Open("mysql", "sophie:qwerty/simplerest?")
+	if err != nil {
+		panic(err)
+	}
+	db = d
 
 }
