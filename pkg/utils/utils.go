55729bdce113ee644634633fe23aad8b0eae7464
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-19 18:54:21 +0300

    unmarshal

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,16 @@
-package main
+package utils
 
-func main() {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
 
 }
