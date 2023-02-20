471ea68a70d2293418c9392a0b08af0deec50c3a
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-20 13:20:05 +0300

    define controllers

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,4 +1,22 @@
-package main
+package controllers
+
+import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/sophiewanjiku/go-bookstore/pkg/utils"
+	"github.com/sophiewanjiku/go-bookstore/pkg/models"
+
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request)  {
+	newBooks :=
+	
+}
 
 func main() {
 
