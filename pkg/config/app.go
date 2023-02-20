74facb4a859ca7b2cfdb3252ed4688f2de4943fe
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-20 13:05:45 +0300

    blahblah

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,16 +1,17 @@
 package config
 
-import (
+import(
 	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/dialects/mysql"
 )
 
-var (
-	db *gorm.DB
+var(
+	db * gorm.DB
 )
 
 func Connect() {
 	d, err := gorm.Open("mysql", "sophie:qwerty/simplerest?charset=utf8&parseTime=True&loc=loclatime")
-	if err != nil {
+	if err != nil{
 		panic(err)
 	}
 	db = d
