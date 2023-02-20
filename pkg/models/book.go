c079a311711ba7c2f79635e8cc8b2a68780c42c0
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-20 13:02:07 +0300

    methods

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,23 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+
+}
